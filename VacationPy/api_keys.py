7676214004158d1d6e65b0b05dd84d1bc6f1cd88
Esther Kim <74941139+estherkim0998@users.noreply.github.com>
2021-01-30 12:07:44 -0800

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "21cc1612f0fa7258b71c17759927eee2"
+weather_api_key = "enter own"
 
 # Google API Key
-g_key = "AIzaSyAMYOq-V-YbYt-p0LjVRcPWwJ-9b7lSozY"
+g_key = "enter own"
